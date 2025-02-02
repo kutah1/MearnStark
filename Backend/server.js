82a220eeac88fd1f109e675629967e57c39f762c
kutah1 <jameskutah47@gmail.com>
2025-02-02 04:29:37 +0000

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
+import dotenv from 'dotenv';
+dotenv.config()
 app.use(express.json())
-app.get('/',(req,res)=>{
+app.get('/products',(req,res)=>{
     res.send("Hello");
 
 })
+console.log(process.env.MONGO_URI)
 app.listen(5000,()=>{
 
   console.log('Server started at port 5000');
